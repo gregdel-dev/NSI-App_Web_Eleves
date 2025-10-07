2e8f358b0f52f6bf006c3fa3892d2ec6aa51824a
Muchas44 <mattiasjv44@gmail.com>
2025-10-07 13:02:15 +0200

    Ajouter base temp

--- Backend.py
+++ Backend.py
@@ -1,11 +1,19 @@
-from flask import Flask,render_template, send_from_directory
+from flask import Flask,render_template, send_from_directory, request
 from os import path
 app=Flask(__name__)
+#Adam: C'est la base temporaire, t'y touche pas. Et au passage dépèche toi gros tahan
+base_temporaire = []
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/ajout')
+    return render_template('index.html',base_temporaire)
+@app.route('/ajout', methods=["POST","GET"])
 def ajout():
+    if request.method=="POST":
+        prenom=request.form["prenom"]
+        nom=request.form["nom"]
+        age=request.form["age"]
+        base_temporaire.append([prenom,nom,age])
+        print(base_temporaire)
     return render_template('ajout.html')
 
 @app.route('/favicon.ico')
