7d5365177361e79f28944829da1254b33519b521
gregdel-dev <moichecke@yahoo.com>
2025-10-07 10:43:52 +0200

    change front

--- Backend.py
+++ Backend.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from os import path
 app=Flask(__name__)
 @app.route('/')
 def home():
     return "Bienvenue sur votre première application Flask !"
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 if __name__=="__main__":
     app.run(debug=True)
--- run_dev.py
+++ run_dev.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     
     server = Server(app.wsgi_app)
     server.watch('*.py')          
-    server.watch('pages/*.html')  
+    server.watch('templates/*.html')  
     server.serve(debug=True, port=5000)
