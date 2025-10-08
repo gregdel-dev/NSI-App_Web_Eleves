64c8e1bc81a920bfd45b5df88445bb70decc3b3d
gregdel-dev <moichecke@yahoo.com>
2025-10-08 09:34:04 +0200

    ajout des fonctions que @adam doit remplir

--- Backend.py
+++ Backend.py
@@ -1,6 +1,30 @@
 from flask import Flask,render_template, send_from_directory, request
 from os import path
 app=Flask(__name__)
+
+#Adam tu devra remplir ces fonctions 
+def creer_eleve(id,prenom,nom,age):
+    return
+
+def lire_eleves():
+    return #renvoie les élèves
+
+def lire_eleve(id):
+    return #renvoie un tuple avec les données de l'élève demandé (par son id)
+
+def supprimer_eleve(id):
+    return 
+
+def update(id, prenom, nom ,age):
+    #si possible tu modifie les données de l'élève (selon son id) sinnon juste tu le supprime et tu le recréé
+    return
+
+
+
+
+
+
+
 #Adam: C'est la base temporaire, t'y touche pas. Et au passage dépèche toi
 base_temporaire = [(1,"prenom1","nom1", 17)]
 @app.route('/')
